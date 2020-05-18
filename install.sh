1c221ae41fa3f5d0bccf8491016622b45d2075f2
slmg <38896727+slmg@users.noreply.github.com>
2020-05-18 02:15:57 +0100

    Implement --hard argument in install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,15 @@ test -d "$HOME" || {
 }
 
 GIT_DIR="$HOME/.config/dotfiles.git"
+GIT_RESET_HARD=false
+
+# Parse arguments
+while [ $# -gt 0 ]; do
+    case $1 in
+        --hard) GIT_RESET_HARD=true ;;
+    esac
+    shift
+done
 
 # Clone the branch dotfiles in `~/.config/dotfiles.git`.
 git clone --bare --branch=dotfiles https://github.com/slmg/dotfiles.git "$GIT_DIR"
@@ -18,3 +27,9 @@ alias dotfiles='git --git-dir=$GIT_DIR --work-tree=$HOME'
 
 # Do not show untracked files when using `dotfiles status`.
 dotfiles config --local status.showUntrackedFiles no
+
+if [ "$GIT_RESET_HARD" = true ]; then
+    echo "'--hard' arg specified: overriding all tracked files in '$HOME'..."
+    dotfiles reset --hard
+    exit 0
+fi
