62a020fbf982529f10a44d58c718bfe6f3319f86
slmg <38896727+slmg@users.noreply.github.com>
2020-05-23 14:13:03 +0100

    Simplify & add sanity checks to vscode sync script

--- .local/bin/vscode-sync-extensions.sh
+++ .local/bin/vscode-sync-extensions.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
+# This script performs two actions:
+#
+#   1. Install all extensions from the extensions list.
+#   2. Dump locally installed extensions to the extensions list.
+#
+# In effect, deleted extensions need to also be deleted from the extensions list.
+# Otherwise they would be installed again the next time this script is run.
+
 set -e
 
+EXTENSIONS_LIST="$HOME/.config/Code/User/extensions.txt"
+
+# Sanity checks.
 command -v code || {
-    echo "VsCode could not be found in PATH, is it installed ?"
+    echo "VsCode could not be found in PATH, is it installed?"
     exit 1
 }
-
-EXTENSIONS_LIST="$HOME/.config/Code/User/extensions.txt"
+test -f "$EXTENSIONS_LIST" || {
+    echo "$EXTENSIONS_LIST does not exist! Aborting..."
+    exit 1
+}
 
 echo "Syncing $EXTENSIONS_LIST..."
 
 # Sync $EXTENSIONS_LIST which may contain new extensions.
 < "$EXTENSIONS_LIST" xargs -L 1 code --install-extension
 
-# Add locally installed extensions to $EXTENSIONS_LIST.
-TMP_EXTENSION_LIST=$(mktemp)
-code --list-extensions >> "$EXTENSIONS_LIST"
-< "$EXTENSIONS_LIST" sort | uniq > "$TMP_EXTENSION_LIST"
-mv "$TMP_EXTENSION_LIST" "$EXTENSIONS_LIST"
+# Overwrite $EXTENSIONS_LIST with locally installed extensions.
+code --list-extensions > "$EXTENSIONS_LIST"
 
 echo "Done."
